7da3e9bf1d927ca69c7138b485dd39f7da80e199
A-med <ahmeddammak91@gmail.com>
2016-05-12 23:55:49 +0100

    firstcommit

---
+++ src/testsonar/TestSonar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsonar;
+
+/**
+ *
+ * @author dmk
+ */
+public class TestSonar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int Abc ;
+        
+        Abc =6;
+        System.out.println(Abc);
+        
+    }
+    
+}
